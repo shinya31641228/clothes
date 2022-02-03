e5a734f234c5a260b2cc31cb840cc831af019a5d
shinya31641228 <s.suzuki@thenewgate.co.jp>
2022-02-03 18:54:37 +0900

    決算処理機能を追加

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use \App\Cart;
+use App\LineItem;
 
 class CartController extends Controller
 {
@@ -25,5 +26,61 @@ class CartController extends Controller
             'line_items' => $cart->products,
             'total_price' => $total_price,
         ]);
+    }
+
+    /**
+     *  決算処理する
+     */
+
+    public function checkout()
+    {
+        //セッションからカートの情報を取得
+        $cart_id = Session::get('cart');
+        $cart = Cart::find($cart_id);
+
+        //カートに商品がなかったらカート画面へリダイレクト
+        if(count($cart->products) <= 0){
+            return redirect(route('cart.index'));
+        }
+
+        //購入商品のセット
+        $line_items = [];
+        foreach ($cart->products as $product) {
+            $line_item = [
+                'name'        => $product->name,
+                'description' => $product->description,
+                'amount'      => $product->price,
+                'currency'    => 'jpy',
+                'quantity'    => $product->pivot->quantity,
+            ];
+            array_push($line_items, $line_item);
+        }
+
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+        $session = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items'           => [$line_items],
+            'success_url'          => route('cart.success'),
+            'cancel_url'           => route('cart.index'),
+        ]);
+
+        return view('cart.checkout', [
+            'session' => $session,
+            'publicKey' => env('STRIPE_PUBLIC_KEY')
+        ]);
+    }
+
+    /**
+     * 決算処理成功時
+     */
+
+    public function success()
+    {
+        //セッションからカートの情報を取得
+        $cart_id = Session::get('cart');
+        LineItem::where('cart_id', $cart_id)->delete();
+
+        return redirect(route('product.index'));
     }
 }
